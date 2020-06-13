b9aefef5024a2c296d99bce111d2e578bd62b2b5
che.lin <rockecstasy@yahoo.com.tw>
2020-06-13 19:00:03 +0800

    新增預訂資訊頁

--- app/js/reservation-received.js
+++
@@ -1,8 +0,0 @@
-(function () {
-  window.onload = function () {
-    const subscribe = document.querySelector('.footer__subscribe');
-
-    // 隱藏 footer 的訂閱功能
-    subscribe.classList.add('d-none');
-  };
-})();
